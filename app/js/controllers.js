05bbe7bb784c8425d5ed5b03db8208a098f04c72
Yehonathan Sharvit <viebel@gmail.com>
2014-01-13 15:02:32 +0200

    bug fix

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,6 @@ angular.module('numbersControllers', []).
 controller('NumbersCtrl', ['$scope',
   function($scope) {
     $scope.$watch('range', function(newVal, oldVal){
-        $scope.numbers = _.range(1, 1*newVal || 100);
+        $scope.numbers = _.range(1, 1 + (newVal? 1*newVal : 100));
     });
   }]);
