077ac876bd132746ea5d36662dcc190bb997f69c
Yehonathan Sharvit <viebel@gmail.com>
2014-01-13 19:32:15 +0200

    highlight divisible numbers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,15 @@
 angular.module('numbersControllers', []).
 controller('NumbersCtrl', ['$scope',
   function($scope) {
-    $scope.$watch('range', function(newVal, oldVal){
-        $scope.numbers = _.range(1, 1 + (newVal? 1*newVal : 100));
+    $scope.range = 200;
+    $scope.divisor = 2;
+    function info(numbers, divisor) {
+        return _.map(numbers, function(x) {
+            return {n: x, divisible: (x % divisor) == 0};
+        });
+    };
+    $scope.$watchCollection('[range, divisor]', function(){
+        $scope.numbers = _.range(1, 1 + 1*$scope.range);
+        $scope.numbersWithInfo = info($scope.numbers, $scope.divisor);
     });
   }]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,8 +2,14 @@
 
 /* Filters */
 
-angular.module('numbersFilters', []).filter('checkmark', function() {
+angular.module('numbersFilters', []).
+filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
+}).
+filter('divisible', function(){
+    return function(items, divisor) {
+        return _.map(items, function(x) { return {n: input, divisible: x % divisor == 0}});
+    }
 });
